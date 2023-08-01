5a632ba951f033c125a3ea7f976caca768b2bb39
Tilakkumar Pardeshi <130922500+Videnta@users.noreply.github.com>
2023-08-01 21:05:19 +0530

    kthterm.cpp

--- kthterm.cpp
+++ kthterm.cpp
@@ -1,27 +1,45 @@
-#include<iostream>
-using namespace std;
+import java.util.PriorityQueue;
+import java.util.Scanner;
 
-int kthLargest(int arr[], int n, int k) {
-  int minIndex, temp;
-  for(int i=0; i<n; i++) {
-    minIndex = i;
-    for(int j=i+1; j<n; j++) {
-      if(arr[minIndex] > arr[j]) {
-        minIndex = j;
-      }
+public class KthLargestElement {
+    public static int findKthLargest(int[] nums, int k) {
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        for (int i = 0; i < k; i++) {
+            minHeap.add(nums[i]);
+        }
+
+        for (int i = k; i < nums.length; i++) {
+
+            if (nums[i] > minHeap.peek()) {
+                minHeap.poll();
+                minHeap.add(nums[i]);
+            }
+        }
+
+        return minHeap.peek();
     }
-    temp = arr[i];
-    arr[i] = arr[minIndex];
-    arr[minIndex] = temp;
-  }
-  return arr[n-k];
-}
 
-int main() {
-  int arr[9] = {2, 7, 1, 3, 8, 4, 6, 9, 5};
-  int size = sizeof(arr)/sizeof(int);
-  int k = 4;
-  cout<<"The "<<k<<"th largest element in the array is "<<kthLargest(arr, size, k);
+    public static void main(String[] args) {
 
-  return 0;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int size = input.nextInt();
+
+        int[] nums = new int[size];
+        System.out.println("Enter " + size + " elements:");
+        for (int i = 0; i < size; i++) {
+            nums[i] = input.nextInt();
+        }
+
+        System.out.print("Enter the value of element k: ");
+        int k = input.nextInt();
+
+        input.close();
+
+        int kthLargest = findKthLargest(nums, k);
+        System.out.println("The " + k + "th largest element is: " + kthLargest);
+    }
 }
